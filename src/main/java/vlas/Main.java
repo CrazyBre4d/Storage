ccb11e1a51a701594cf692f7e4d541872950874c
CrazyBre4d <93737550+CrazyBre4d@users.noreply.github.com>
2023-08-24 11:08:11 +0300

    First commit

---
+++ src/main/java/vlas/Main.java
@@ -0,0 +1,43 @@
+package vlas;
+
+import vlas.entity.Storage;
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+
+public class Main {
+    public static void main(String[] args) {
+        try{
+        int numberForSearch = 31;
+        Storage<Integer> intStorage = new Storage<>(5);
+
+        intStorage.set(0, 1);
+        intStorage.set(1, null);
+        intStorage.add(112);
+        intStorage.set(3, 133);
+        intStorage.set(4, 12);
+
+        System.out.println(intStorage);
+
+        intStorage.find(112);
+        intStorage.sort();
+
+        System.out.println(intStorage);
+
+        Comparator<Integer> ascendingComparator = Integer::compareTo;
+        int index = intStorage.binarySearch(numberForSearch, ascendingComparator);
+        String found = String.format("Element detected: %d", index);
+        intStorage.binarySearch(numberForSearch,ascendingComparator);
+
+        if (index != -1) {
+            System.out.println(found);
+        } else {
+            System.out.println("No such element");
+        }
+        } catch (IndexOutOfBoundsException | NoSuchElementException | NullPointerException e) {
+            System.out.println(e);
+        }
+    }
+}
+
+
+
---
+++ src/main/java/vlas/entity/Storage.java
@@ -0,0 +1,114 @@
+package vlas.entity;
+
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+
+public class Storage<T extends Number>  {
+    private T[] data;
+
+    public Storage() {}
+
+    public Storage(T[] initialArray) {
+        data = Arrays.copyOf(initialArray, initialArray.length);
+    }
+
+    public Storage(int capacity) {
+        data = (T[]) new Number[capacity];
+    }
+
+    public void set(int index, T value) {
+        if (index >= 0 && index < data.length) {
+            data[index] = value;
+        } else {
+           throw new IndexOutOfBoundsException("set: No such cell exists");
+        }
+    }
+
+    public T get(int index) {
+        if ((index >= 0 && index < data.length)) {
+            return data[index];
+        } else {
+            throw new NoSuchElementException("get: No such cell exists");
+        }
+    }
+
+    public void remove(int index) {
+        if (index >= 0 && index < data.length) {
+            data[index] = null;
+        } else {
+            throw new NoSuchElementException(" remove: No such cell exists");
+        }
+    }
+
+    public void add(T value) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == null) {
+                data[i] = value;
+                return;
+            }
+        }
+        T[] newData = Arrays.copyOf(data, data.length + 1);
+        newData[newData.length - 1] = value;
+        data = newData;
+    }
+
+    public void find(T value) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == value) {
+                System.out.println("Detected! It's place: " + i);
+                break;
+
+            } //throw new NoSuchElementException("find: No such cell exists");
+        }
+        }
+
+
+    public void sort() {
+        boolean swapped;
+        do {
+            swapped = false;
+            for (int i = 0; i < data.length - 1; i++) {
+                if (data[i] != null && data[i + 1] != null && data[i].doubleValue() > data[i + 1].doubleValue()) {
+                    T temp = data[i];
+                    data[i] = data[i + 1];
+                    data[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+        } while (swapped);
+    }
+    public int binarySearch(T target, Comparator<T> comparator) {
+        for (int i = 0; i < data.length; i++) {
+             if (data[i] == null) {
+                 throw new NullPointerException("binarySearch: Fill the array");}}
+
+        int left = 0;
+        int right = data.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int comparison = comparator.compare(data[mid], target);
+
+            if (comparison == 0) {
+                return mid;
+            } else if (comparison < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+    public int length() {
+        return data.length;
+    }
+
+    @Override
+    public String toString() {
+        return "Storage{" +
+                "data=" + Arrays.toString(data) +
+                '}';
+    }
+}
